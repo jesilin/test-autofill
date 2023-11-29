801e8007677be735a04487da8ad2cc05a45a3fca
jesilin <55233577+jesilin@users.noreply.github.com>
2023-11-29 19:14:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => {
+  console.log(`Receive GET request`);
+  return res.type('html').send(html);
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
